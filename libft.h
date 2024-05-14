990bd394bb208a8224277cf2abfb2ee0ed505b0e
root <sushi.honda@gmail.com>
2024-05-14 15:54:29 +0800

    Add ft_memset prototype declaration

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 13:59:59 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/14 15:53:43 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@
 
 size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 #endif // LIBFT_H
