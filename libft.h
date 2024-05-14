c8e5f950a7e930d102150bda98aa9540d97a541c
root <sushi.honda@gmail.com>
2024-05-14 13:44:00 +0800

    Add ft_bzero prototype declaration

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 07:47:37 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/14 13:41:53 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # include <unistd.h>
 # include <stdio.h>
 
-size_t	strlen(const char *s);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t);
 #endif // LIBFT_H
