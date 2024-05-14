4b88c74b3a3f71404eda1d5cfc941bf001adc6c4
root <sushi.honda@gmail.com>
2024-05-14 14:15:42 +0800

    typo revised

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 13:41:53 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/14 13:59:59 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@
 # include <stdio.h>
 
 size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t);
+void	ft_bzero(void *s, size_t n);
 #endif // LIBFT_H
