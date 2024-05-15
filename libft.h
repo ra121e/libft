6872079e24e4762350ec1a21e5a790180a69a139
root <sushi.honda@gmail.com>
2024-05-16 00:32:39 +0800

    Add strrchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/15 21:30:32 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/15 21:41:35 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
 #endif // LIBFT_H
