2aaf357354cb77130964a0a43d124863dbf1b30d
root <sushi.honda@gmail.com>
2024-05-15 20:39:29 +0800

    Add ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/15 18:07:31 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/15 20:39:07 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 #endif // LIBFT_H
