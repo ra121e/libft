af200cd91ce63c88a763c3f0947fa21059431b48
root <sushi.honda@gmail.com>
2024-05-17 11:19:01 +0800

    Revised for Makefile

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 10:31:59 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 14:22:10 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/17 11:17:47 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,4 @@ void	ft_bzero(void *s, size_t n)
 		*(ptr + i) = '\0';
 		i++;
 	}
-}
-
-#include <stdio.h>
-#include <strings.h>
-
-int	main(void)
-{
-	int		a;
-	size_t	i;
-	char	str[] = "ThisisString";
-
-	a = 0;
-	i = 0;
-	printf("%s\n", str);
-	ft_bzero(str, i);
-	while (a <= 12)
-	{
-		printf("%c\n", str[a]);
-		a++;
-	}
-	return (0);
 }
