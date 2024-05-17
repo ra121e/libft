42ab5f9b2b9f4d34cd57675b38a5d36c5ccf6ab8
root <sushi.honda@gmail.com>
2024-05-17 11:20:03 +0800

    Add ft_bzero for Makefile

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/17 10:44:19 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/17 11:16:53 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,25 @@
 #include <string.h>
 #include "libft.h"
 
-size_t	ft_strlen(const char *s);
-
 int	main(void)
 {
-	char	a[19] = "length test";
+	char	sla[19] = "length test";
 
-	printf("return value from ft_strlen %ld\n", ft_strlen(a));
-	printf("return value from original strlen %ld\n", strlen(a));
+	printf("return value from ft_strlen %ld\n", ft_strlen(sla));
+	printf("return value from original strlen %ld\n", strlen(sla));
+
+	int		bza;
+	size_t	bzi;
+	char	bzstr[] = "ThisisString";
+
+	bza = 3;
+	bzi = 0;
+	printf("%s\n", bzstr);
+	ft_bzero(bzstr, bza);
+	while (bzi <= 12)
+	{
+		printf("%c\n", bzstr[bzi]);
+		bzi++;
+	}
 	return (0);
 }
