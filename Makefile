f6e7e94da407bca4da028aa25d7bd142d757ab26
root <sushi.honda@gmail.com>
2024-05-17 10:52:32 +0800

    1st Makefile with ft_strlen

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/14 07:47:54 by athonda           #+#    #+#              #
-#    Updated: 2024/05/15 10:02:49 by athonda          ###   ########.fr        #
+#    Updated: 2024/05/17 10:48:44 by athonda          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 libft.a: ft_strlen.c
-		gcc -Wall -Wextra -Werror -o libft.a ft_strlen.c
-
+		gcc -Wall -Wextra -Werror -c ft_strlen.c
+		ar rc libft.a ft_strlen.o
 clean:
-		rm -f libft.a
+		rm -f libft.a ft_strlen.o
--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/17 09:32:28 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:44:19 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
-
+size_t	ft_strlen(const char *s);
 
 int	main(void)
 {
@@ -23,5 +23,5 @@ int	main(void)
 
 	printf("return value from ft_strlen %ld\n", ft_strlen(a));
 	printf("return value from original strlen %ld\n", strlen(a));
-	return (-1);
+	return (0);
 }
