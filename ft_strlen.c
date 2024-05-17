73ebb5a8da80d887056e6f1f9cfe1835c59b6bce
root <sushi.honda@gmail.com>
2024-05-17 09:41:53 +0800

    revised for the test Makefile

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:46:03 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 10:17:07 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/17 09:32:43 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,4 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-#include <stdio.h>
-#include <string.h>
-
-int	main(void)
-{
-	char	a[20] = "length test";
-
-	printf("return value from ft_strlen %ld\n", ft_strlen(a));
-	printf("return value from original strlen %ld\n", strlen(a));
-	return (0);
 }
--- main.c
+++ main.c
@@ -6,162 +6,22 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/14 07:47:47 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/17 09:32:28 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
+#include "libft.h"
 
-int ft_strlen(char *str);
-int ft_isalpha(char c);
-int ft_isdigit(char c);
-int	ft_isalnum(unsigned char c);
-int	ft_isascii(char c);
-int	ft_isprint(char c);
-int ft_toupper(int c);
-int ft_tolower(int C);
+size_t	ft_strlen(const char *s)
 
-int main(int ac, char *av[])
+
+int	main(void)
 {
-	int		len;
-	char	alp;
-	char	org_alp;
-	char	dig;
-	char	org_dig;
+	char	a[19] = "length test";
 
-	if (ac == 1)
-	{
-	return (0);
-	}
-	len = ft_strlen(av[1]);
-	printf ("The length of string is: %d\n", len);
-
-// test of original isalpha()
-	alp = 'A';
-	org_alp = isalpha(alp);
-	printf("value returned from isalpha as A: %d\n", org_alp);
-	alp = 'a';
-	org_alp = isalpha(alp);
-	printf("value returned from isalpha as a: %d\n", org_alp);
-// OMG, it returns 1 for capital letter, 2 for lowercase..
-// test of my ft_isalpha()
-	alp = 'A';
-	org_alp = ft_isalpha(alp);
-	printf("value returned from my ft_isalpha as A: %d\n", org_alp);
-	alp = 'a';
-	org_alp = isalpha(alp);
-	printf("value returned from my ft_isalpha as a: %d\n", org_alp);
-
-// test of original isdigit()
-	dig = '6';
-	org_dig = isdigit(dig);
-	printf("value returned from isdigit as 6: %d\n", org_dig);
-// test of my _ftisdigit()
-	dig = '6';
-	org_dig = ft_isdigit(dig);
-	printf("value returned from my ft_isdigit as 6: %d\n", org_dig);
-
-// test of original isalnum()
-	alp = 65;
-	org_alp = isalnum(alp);
-	printf("value returned from isalnum as A: %d\n", org_alp);
-	alp = 97;
-	org_alp = isalnum(alp);
-	printf("value returned from isalnum as a: %d\n", org_alp);
-	dig = '6';
-	org_dig = isalnum(dig);
-	printf("value returned from isalnum as digit 6: %d\n", org_dig);
-// test of my ft_isalnum()
-	alp = 65;
-	org_alp = ft_isalnum(alp);
-	printf("value returned from my ft_isalnum as A: %d\n", org_alp);
-	alp = 97;
-	org_alp = ft_isalnum(alp);
-	printf("value returned from my ft_isalnum as a: %d\n", org_alp);
-	dig = '6';
-	org_dig = ft_isalnum(dig);
-	printf("value returned from my ft_isalnum as digit 6: %d\n", org_dig);
-
-// test of original isascii()
-	alp = 0;
-	org_alp = isascii(alp);
-	printf("value returned from isascii as A: %d\n", org_alp);
-	alp = 127;
-	org_alp = isascii(alp);
-	printf("value returned from isascii as a: %d\n", org_alp);
-	dig = 128;
-	org_dig = isascii(dig);
-	printf("value returned from isascii as digit 6: %d\n", org_dig);
-// test of my ft_isascii()
-	alp = 0;
-	org_alp = ft_isascii(alp);
-	printf("value returned from ft_isascii as A: %d\n", org_alp);
-	alp = 127;
-	org_alp = ft_isascii(alp);
-	printf("value returned from ft_isascii as a: %d\n", org_alp);
-	dig = 128;
-	org_dig = ft_isascii(dig);
-	printf("value returned from ft_isascii as digit 6: %d\n", org_dig);
-// test of original isprint()
-	alp = 31;
-	org_alp = isprint(alp);
-	printf("value returned from isprint as A: %d\n", org_alp);
-	alp = 32;
-	org_alp = isprint(alp);
-	printf("value returned from isprint as a: %d\n", org_alp);
-	dig = 127;
-	org_dig = isprint(dig);
-	printf("value returned from isprint as digit 6: %d\n", org_dig);
-// test of my ft_isprint()
-	alp = 31;
-	org_alp = ft_isprint(alp);
-	printf("value returned from my ft_isprint as A: %d\n", org_alp);
-	alp = 32;
-	org_alp = ft_isprint(alp);
-	printf("value returned from my ft_isprint as a: %d\n", org_alp);
-	dig = 127;
-	org_dig = ft_isprint(dig);
-	printf("value returned from my ft_isprint as digit 6: %d\n", org_dig);
-// test of original toupper()
-	alp = 'a';
-	org_alp = toupper(alp);
-	printf("value returned from toupper as A: %c\n", org_alp);
-	alp = 'A';
-	org_alp = toupper(alp);
-	printf("value returned from toupper as a: %c\n", org_alp);
-	dig = '8';
-	org_dig = toupper(dig);
-	printf("value returned from toupper as digit 6: %c\n", org_dig);
-// test of my ft_toupper()
-	alp = 'a';
-	org_alp = ft_toupper(alp);
-	printf("value returned from ft_toupper as A: %c\n", org_alp);
-	alp = 'A';
-	org_alp = ft_toupper(alp);
-	printf("value returned from ft_toupper as a: %c\n", org_alp);
-	dig = '8';
-	org_dig = ft_toupper(dig);
-	printf("value returned from ft_toupper as digit 6: %c\n", org_dig);
-// test of original tolower()
-	alp = 'A';
-	org_alp = tolower(alp);
-	printf("value returned from tolower as A: %c\n", org_alp);
-	alp = 'a';
-	org_alp = tolower(alp);
-	printf("value returned from tolower as a: %c\n", org_alp);
-	dig = '8';
-	org_dig = tolower(dig);
-	printf("value returned from tolower as digit 6: %c\n", org_dig);
-// test of my ft_tolower()
-	alp = 'A';
-	org_alp = ft_tolower(alp);
-	printf("value returned from ft_tolower as A: %c\n", org_alp);
-	alp = 'a';
-	org_alp = ft_tolower(alp);
-	printf("value returned from ft_tolower as a: %c\n", org_alp);
-	dig = '8';
-	org_dig = ft_tolower(dig);
-	printf("value returned from ft_tolower as digit 6: %c\n", org_dig);
-	return (0);
+	printf("return value from ft_strlen %ld\n", ft_strlen(a));
+	printf("return value from original strlen %ld\n", strlen(a));
+	return (-1);
 }
