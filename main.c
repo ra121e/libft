df2ca9a50ba623d20e1c060177e6bbf57e938487
root <sushi.honda@gmail.com>
2024-05-20 11:14:52 +0800

    revise bzero test

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/19 19:41:06 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/20 11:12:53 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <bsd/string.h>
@@ -52,25 +53,71 @@ void	t_strlen(void)
 	printf("return value from original strlen %ld\n", strlen(sla));
 }
 
+// テストケースを実行する関数
+void run_bzero_test(const char *input_label, unsigned char *input, size_t size) {
+    unsigned char buffer_bzero[size];
+    unsigned char buffer_ft_bzero[size];
+
+    // bzero を実行
+    memcpy(buffer_bzero, input, size);
+    bzero(buffer_bzero, size);
+
+    // ft_bzero を実行
+    memcpy(buffer_ft_bzero, input, size);
+    ft_bzero(buffer_ft_bzero, size);
+
+    // 結果を出力
+    printf("Input: %s\n", input_label);
+    printf("Expected (bzero): ");
+    for (size_t i = 0; i < size; ++i) {
+        printf("%02x ", buffer_bzero[i]);
+    }
+    printf("\n");
+    printf("Actual (ft_bzero):   ");
+    for (size_t i = 0; i < size; ++i) {
+        printf("%02x ", buffer_ft_bzero[i]);
+    }
+    printf("\n");
+
+    // 結果の比較
+    if (memcmp(buffer_bzero, buffer_ft_bzero, size) == 0) {
+        printf("Result: PASS\n\n");
+    } else {
+        printf("Result: FAIL\n\n");
+    }
+}
+
+
 void	t_bzero(void)
 {
-	// ft_bzero test
-	int		bza;
-	size_t	bzi;
-	char	bzstr[] = "ThisisString";
 
-	bza = 3;
-	bzi = 0;
-	printf("%s\n", bzstr);
-	ft_bzero(bzstr, bza);
-	while (bzi <= 12)
-	{
-		printf("%c\n", bzstr[bzi]);
-		bzi++;
-	}
+    printf("Prototyping:\nvoid bzero(void *s, size_t n);\n\n");
 
+    unsigned char input1[] = { 'a', 'b', 'c', 'd', 'e' };
+    unsigned char input2[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    unsigned char input3[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+    unsigned char input4[] = { 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q' };
+    unsigned char input5[] = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' };
+    unsigned char input6[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    unsigned char input7[] = { '-', '=', '[', ']', ';', '\'', '\\', ',', '.', '/' };
+    unsigned char input8[] = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' };
+    unsigned char input9[] = { 'm', 'n', 'b', 'v', 'c', 'x', 'z', 'a', 's', 'd' };
+    unsigned char input10[] = { '!', '1', 'A', 'b', '2', '@', 'C', 'c', '3', '#' };
+
+    // テストケースを実行
+    run_bzero_test("abcde", input1, sizeof(input1));
+    run_bzero_test("1234567890", input2, sizeof(input2));
+    run_bzero_test("ABCDEFGHIJ", input3, sizeof(input3));
+    run_bzero_test("zyxwvutsrq", input4, sizeof(input4));
+    run_bzero_test("!@#$%^&*()", input5, sizeof(input5));
+    run_bzero_test("0123456789", input6, sizeof(input6));
+    run_bzero_test("-=[];',./", input7, sizeof(input7));
+    run_bzero_test("qwertyuiop", input8, sizeof(input8));
+    run_bzero_test("mnbvcxzasd", input9, sizeof(input9));
+    run_bzero_test("!1Ab2@Cc3#", input10, sizeof(input10));
 
 }
+
 void	t_memset(void)
 {
 
