affb18412237e5b769629722f4616ae794757dce
root <sushi.honda@gmail.com>
2024-05-21 19:54:59 +0800

    It worksgit add ft_putnbr_fd.c  but I cannot get why minus case

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:36:07 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/21 16:22:03 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/21 19:50:00 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 10)
-		ft_putchar_fd(n % 10)
 	if (n == -2147483618)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putchar_fd('2', fd);
 		ft_putnbr_fd(147483648, fd);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-n, fd);
+		n = n * (-1);
 	}
 	if (n >= 10)
-	ft_putnbr_fd(n / 10, fd)
-	ft_putchar_fd(n % 10, fd)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	ft_putchar_fd('0' + (n % 10), fd);
 }
