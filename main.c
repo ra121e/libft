ed085ca1e69f6285690345733b81ea3fd5010d73
root <sushi.honda@gmail.com>
2024-05-21 13:33:30 +0800

    Test ft_substr with Jason tester coooool

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 07:47:45 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/20 23:37:07 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/21 13:26:40 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 #include <assert.h>
 #include <bsd/string.h>
 #include "libft.h"
+
+// Helper function to clear the screen
+void clearScreen() {
+	system("clear||cls");
+}
 
 void	t_isdigit(void)
 {
@@ -465,6 +470,89 @@ void	t_strdup(void)
 
 }
 
+
+void	t_substr(void)
+{
+	clearScreen();
+	printf("Function: char *ft_substr(char const *s, unsigned int start, size_t len)\n");
+	printf("Description: The ft_substr() function allocates and returns a substring from the string 's'. The substring begins at index 'start' and is of maximum size 'len'.\n");
+	printf("Input: A pointer to a null-terminated string, the starting index, and the maximum length of the substring\n");
+	printf("Output: A pointer to the newly allocated substring, or NULL if the allocation fails\n\n");
+
+	char str1[] = "hello, world";
+	printf("Test Case 1: s = \"hello, world\", start = 0, len = 5\n");
+	printf("Expected Output: \"hello\"\n");
+	char *result1 = ft_substr(str1, 0, 5);
+	if (result1 != NULL) {
+		printf("Result: %s\n\n", result1);
+		free(result1);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+
+	char str2[] = "hello, world";
+	printf("Test Case 2: s = \"hello, world\", start = 7, len = 5\n");
+	printf("Expected Output: \"world\"\n");
+	char *result2 = ft_substr(str2, 7, 5);
+	if (result2 != NULL) {
+		printf("Result: %s\n\n", result2);
+		free(result2);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+
+	char str3[] = "hello, world";
+	printf("Test Case 3: s = \"hello, world\", start = 0, len = 100\n");
+	printf("Expected Output: \"hello, world\"\n");
+	char *result3 = ft_substr(str3, 0, 100);
+	if (result3 != NULL) {
+		printf("Result: %s\n\n", result3);
+		free(result3);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+
+	char str4[] = "hello, world";
+	printf("Test Case 4: s = \"hello, world\", start = 20, len = 5\n");
+	printf("Expected Output: \"\"\n");
+	char *result4 = ft_substr(str4, 20, 5);
+	if (result4 != NULL) {
+		printf("Result: %s\n\n", result4);
+		free(result4);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+
+	char str5[] = "hello, world";
+	printf("Test Case 5: s = \"hello, world\", start = 5, len = 0\n");
+	printf("Expected Output: \"\"\n");
+	char *result5 = ft_substr(str5, 5, 0);
+	if (result5 != NULL) {
+		printf("Result: %s\n\n", result5);
+		free(result5);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+
+	printf("Enter a string to test: ");
+	char customStr[100];
+	scanf(" %99[^\n]", customStr);
+	printf("Enter the starting index: ");
+	unsigned int start;
+	scanf("%u", &start);
+	printf("Enter the maximum length: ");
+	size_t len;
+	scanf("%zu", &len);
+	char *customResult = ft_substr(customStr, start, len);
+	if (customResult != NULL) {
+		printf("Result: %s\n\n", customResult);
+		free(customResult);
+	} else {
+		printf("Result: NULL\n\n");
+	}
+}
+
+
 int	main(void)
 {
 
@@ -477,7 +565,7 @@ int	main(void)
 //t_bzero();
 //t_memset();
 //t_memcpy();
-t_memmove();
+//t_memmove();
 //t_memchr();
 //t_memcmp();
 //t_strchr();
@@ -489,6 +577,6 @@ t_memmove();
 //t_atoi();
 //t_calloc();
 //t_strdup();
-
+t_substr();
 	return (0);
 }
