7bfd92b1ac1709b0fd710b90f8913e74e6c9a84b
root <sushi.honda@gmail.com>
2024-05-22 08:55:43 +0800

    Minor revise comment

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,25 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 22:06:46 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/18 22:36:31 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/21 23:19:12 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-/*
-	allocate dynamic memory
-	To create several sizes of array
+/**
+ * @file ft_calloc.c
+ * @brief allocate dynamic memory to create several sizes of array
 */
 
 #include "libft.h"
 
-/*
-	Ordinally array must be declared with static size
+/**
+ * @fn void *ft_calloc(size_t nmemb, size_t size)
+ * @brief
+ * @param[in] nmemb is unit of memory, like char, int
+ * @param[in] size is the number of units. how many block in address
+ * @return void type pointer, because no need to specify the type
+ * @note Ordinally array must be declared with static size
 	In case the size depends on the argument, dynamic memory needed
-
-
 */
 
 void	*ft_calloc(size_t nmemb, size_t size)
