e0319d51bba2633f175550b9c4429b76808174e7
root <sushi.honda@gmail.com>
2024-05-24 15:39:32 +0800

    Add cast to char, delete NULL check

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 21:09:38 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/22 13:33:59 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/24 15:38:55 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,20 @@
  * @fn char	*ft_strchr(const char *s, int c)
  * @brief to find a first letter in the text
  * @param[in]	s string to be checked in
- * @param[in]	c character (1 byte)
- * @return pointer to the matched character or NULL
+ * @param[in]	c character (1 byte) but type int...
+ * @return pointer to the matched character or NULL by char pointer type
+ * @attention to compare each letters type should be casted
 */
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
-	if (s == NULL)
-		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			return ((void *)&s[i]);
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
 		i++;
 	}
 	return (NULL);
