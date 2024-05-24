ec909244e29e0e6704f28ee8417e8745b6336694
root <sushi.honda@gmail.com>
2024-05-24 15:19:24 +0800

    revise type: unsigned char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,20 +6,20 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 20:43:46 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/15 21:01:19 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/24 15:18:47 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	const char *str1;
-	const char *str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	str1 = (const char *)s1;
-	str2 = (const char *)s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
@@ -28,24 +28,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		else
 			return (str1[i] - str2[i]);
 	}
-	return (0);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	str1[] = "42singapore is located in east";
-	char	str2[] = "42singabooooo";
-	int		i;
-
-	i = 4;
-	printf("%s\n", str1);
-	printf("%s\n", str2);
-	printf("%d\n", ft_memcmp(str1, str2, i));
-	i = 10;
-	printf("%d\n", ft_memcmp(str1, str2, i));
-	i = 0;
-	printf("%d\n", ft_memcmp(str1, str2, i));
 	return (0);
 }
