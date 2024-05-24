b0b4f40ea8f0bc0daa75e4fa28ba62ca989db30e
root <sushi.honda@gmail.com>
2024-05-24 14:31:55 +0800

    Add comment, cast and type unsigned char

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,45 +6,42 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 19:10:14 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/15 20:03:37 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/24 14:31:00 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file ft_memchr.c
+ * @brief scan memory for a character
+*/
+
 #include <stdio.h>
+
+/**
+ * @fn void	*ft_memchr(const void *s, int c, size_t n)
+ * @brief search for the first occurrence of value in memory area
+ * @param[in]	s pointer to the memory area to search
+ * @param[in]	c serach letter
+ * @param[in]	n bytes to be searched
+ * @return pointer to the address of the value, is not NULL
+ * @attention No NULL check!!
+ * @note cast c as unsigned char because s is casted to undigned already
+*/
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	const char	*p;
+	size_t			i;
+	unsigned char	*p;
 
-	p = (const char *)s;
+	p = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (p[i] == c)
+		if (p[i] == (unsigned char *)c)
 		{
 			return ((void *)&p[i]);
 		}
 		i++;
 	}
 	return (NULL);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	str[] = "This is a 42 premise";
-	char	str2[] = "sjfqoejwjo4351";
-	char	c;
-
-	c = 'a';
-	printf("%s\n", str);
-	printf("%p\n", str);
-	printf("%s\n", str2);
-	printf("%p\n", str2);
-	printf("%p\n", ft_memchr(str, c, 10));
-	printf("%p\n", ft_memchr(str, c, 6));
-	printf("%p\n", ft_memchr(str2, c, 10));
-	return (0);
 }
