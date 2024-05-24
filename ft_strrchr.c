2cdf47eae50a9c1985a19761ae3ebd0f781805c3
root <sushi.honda@gmail.com>
2024-05-24 15:54:52 +0800

    delete main

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 21:34:18 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/15 21:40:20 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/24 15:44:25 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,9 @@ char	*ft_strrchr(const char *s, int c)
 	p = NULL;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			p = (void *)&s[i];
+		if (s[i] == (char)c)
+			p = (char *)&s[i];
 		i++;
 	}
 	return (p);
-}
-
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	str[] = "This is a 42 art";
-	char	str2[] = "sjfqoejwjo4351";
-	char	c;
-
-	c = 'a';
-	printf("%s\n", str);
-	printf("%p\n", str);
-	printf("%s\n", str2);
-	printf("%p\n", str2);
-	printf("%p\n", ft_strrchr(str, c));
-	printf("%p\n", ft_strrchr(str2, c));
-	c = '\0';
-	printf("%p\n", ft_strrchr(str, c));
-	return (0);
 }
