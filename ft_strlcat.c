4d5415a79a4aedfc66d23559ce90da04777e7d44
root <sushi.honda@gmail.com>
2024-05-25 18:52:27 +0800

    bug fix: typo 1 -> i

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 13:05:54 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/20 16:15:27 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/25 18:43:16 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,27 @@
  * @param[in] size total size of dst including NULL terminator
  * @return total of dst plus src
  * @retval total of size and src in case dst is larger than size
+ * @attention strlcat needed to include as bsd/string.h for libbsd
  * @note	because we want to put the NUL terminate
  * 			size should be dest + src + 1
- * @attention strlcat needed to include as bsd/string.h for libbsd
 */
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
+	size_t	src_len;
 	size_t	i;
 
 	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	if (size <= dst_len)
-		return (size + ft_strlen(src));
+		return (size + src_len);
 	i = 0;
-	while (dst_len + 1 < size - 1 && src[i] != '\0')
+	while (dst_len + i < size - 1 && src[i] != '\0')
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (dst_len + ft_strlen(src));
+	return (dst_len + src_len);
 }
