cecbd6e8bb97cc71d36656226f591c43674f8fb9
root <sushi.honda@gmail.com>
2024-05-26 18:29:38 +0800

    Norm v1

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,13 +6,13 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 08:46:11 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/24 12:12:37 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/26 18:29:15 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
