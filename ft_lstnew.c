75948aec5a67d908a357572ced3da95a3c5c20dd
root <sushi.honda@gmail.com>
2024-05-27 11:50:00 +0800

    1st

---
+++ ft_lstnew.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 09:38:24 by athonda           #+#    #+#             */
+/*   Updated: 2024/05/27 10:51:35 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file ft_lstnew.c
+ * @brief allocate and return new node
+*/
+
+#include "libft.h"
+
+/**
+ * @fn l_list	*ft_lstnew(void *content)
+ * @brief make memory area by malloc and set content in the member
+ * @param[in]	content the pointer to be set in the list box
+ * @return pointer to the beginning of the list
+ * @sa malloc
+ * @note set NULL in the next member. its used for linking to other box
+*/
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = (t_list *)malloc(sizeof (t_list) * 1);
+	if (!ptr)
+		return (NULL);
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
+}
