4f1fb6d98bd767acdf2f56f147c62b6a7b740cd3
root <sushi.honda@gmail.com>
2024-05-28 14:29:43 +0800

    Comment revise: What is lstnew? storering contentgit add ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 09:38:24 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/27 10:51:35 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/28 14:28:42 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@
 /**
  * @fn l_list	*ft_lstnew(void *content)
  * @brief make memory area by malloc and set content in the member
- * @param[in]	content the pointer to be set in the list box
+ * @param[in]	content any pointer to be set in the list box
  * @return pointer to the beginning of the list
  * @sa malloc
- * @note set NULL in the next member. its used for linking to other box
+ * @note
+	- 1)malloc make 1 size of t_list and cast to t_list type
+	- 2)store content in content place
+	- 3)set NULL in the next member. its used for linking to other box
 */
 
 t_list	*ft_lstnew(void *content)
