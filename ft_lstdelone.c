7376229d86133b482b7c1c5ba244cdd7552ae0aa
root <sushi.honda@gmail.com>
2024-05-28 11:29:03 +0800

    Minor revise: reject handling next pointer

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 19:13:36 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/27 20:39:18 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/28 11:27:52 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,15 @@
  * @param[in]	lst the address of the node to be free
  * @param[in,out]	fnction (*del) applied to the node
  * @return none
+ * @note reject: evacuate next address to temp. no need in this function
 */
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list	*tmp;
-
 	if (lst == NULL)
 		return ;
-	tmp = lst->next;
 	if (del == NULL)
 		return ;
 	(*del)(lst->content);
 	free(lst);
-	lst = tmp;
 }
