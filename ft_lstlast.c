f4dcac25889c8d366f021f2675fa5fe8fbe5c21f
root <sushi.honda@gmail.com>
2024-05-28 15:19:14 +0800

    Comment revise: lstlast is Node function

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 16:16:46 by athonda           #+#    #+#             */
-/*   Updated: 2024/05/27 16:42:32 by athonda          ###   ########.fr       */
+/*   Updated: 2024/05/28 15:18:13 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@
 #include "libft.h"
 
 /**
- * @fn l_list	*ft_lstlast(t_list *lst)
+ * @fn t_list	*ft_lstlast(t_list *lst)
  * @brief
  * @param[in]	lst pointer to the first box
  * @return pointer to the last box
+ * @note lstlast is focusing on Node(Link List Rule)
+	- 1st node is the clue To reach the last node
+	- gives us node address same as lstnew
 */
 
 t_list	*ft_lstlast(t_list *lst)
